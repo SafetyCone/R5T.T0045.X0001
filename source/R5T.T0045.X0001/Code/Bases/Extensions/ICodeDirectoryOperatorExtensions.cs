e8b80e17cc18c132fbd6a9ad0ab5bfe4c6290ebd
David Coats <david@getsafetycone.com>
2021-10-05 22:21:32 -0700

    Add required project reference dependencies. Add ICodeDirectoryOperator extensions.

---
+++ source/R5T.T0045.X0001/Code/Bases/Extensions/ICodeDirectoryOperatorExtensions.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Linq;
+
+using R5T.T0045;
+
+using Instances = R5T.T0045.X0001.Instances;
+
+
+namespace System
+{
+    public static class ICodeDirectoryOperatorExtensions
+    {
+        public static string[] GetServiceClassesDirectoryFilePaths(this ICodeDirectoryOperator _,
+            string projectDirectoryPath)
+        {
+            var projectServiceClassesDirectoryPath = Instances.ProjectPathsOperator.GetServiceClassesDirectoryPath(projectDirectoryPath);
+
+            var serviceClassesDirectoryFilePaths = Instances.FileSystemOperator.EnumerateAllDescendentFilePathsOrEmptyIfNotExists(projectServiceClassesDirectoryPath);
+
+            var output = serviceClassesDirectoryFilePaths.ToArray();
+            return output;
+        }
+
+        public static Func<string, string[]> GetServiceClassesDirectoryFilePathsGenerator(this ICodeDirectoryOperator _)
+        {
+            string[] Output(string projectDirectoryPath) => _.GetServiceDefinitionDirectoryFilePaths(projectDirectoryPath);
+            return Output;
+        }
+
+        public static string[] GetServiceDefinitionDirectoryFilePaths(this ICodeDirectoryOperator _,
+            string projectDirectoryPath)
+        {
+            var projectServiceDefinitionsDirectoryPath = Instances.ProjectPathsOperator.GetServiceDefinitionsDirectoryPath(projectDirectoryPath);
+
+            var serviceDefinitionDirectoryFilePaths = Instances.FileSystemOperator.EnumerateAllDescendentFilePathsOrEmptyIfNotExists(projectServiceDefinitionsDirectoryPath);
+
+            var output = serviceDefinitionDirectoryFilePaths.ToArray();
+            return output;
+        }
+
+        public static Func<string, string[]> GetServiceDefinitionDirectoryFilePathsGenerator(this ICodeDirectoryOperator _)
+        {
+            string[] Output(string projectDirectoryPath) => _.GetServiceDefinitionDirectoryFilePaths(projectDirectoryPath);
+            return Output;
+        }
+
+        public static string[] GetServiceImplementationDirectoryFilePaths(this ICodeDirectoryOperator _,
+            string projectDirectoryPath)
+        {
+            var projectSerivceImplementationDirectoryPath = Instances.ProjectPathsOperator.GetServiceImplementationsDirectoryPath(projectDirectoryPath);
+
+            var serviceImplementationDirectoryFilePaths = Instances.FileSystemOperator.EnumerateAllDescendentFilePathsOrEmptyIfNotExists(projectSerivceImplementationDirectoryPath);
+
+            var output = serviceImplementationDirectoryFilePaths.ToArray();
+            return output;
+        }
+
+        public static Func<string, string[]> GetServiceImplementationDirectoryFilePathsGenerator(this ICodeDirectoryOperator _)
+        {
+            string[] Output(string projectDirectoryPath) => _.GetServiceImplementationDirectoryFilePaths(projectDirectoryPath);
+            return Output;
+        }
+
+        public static string[] GetServiceInterfaceDirectoryFilePaths(this ICodeDirectoryOperator _,
+            string projectDirectoryPath)
+        {
+            var projectServiceInterfacesDirectoryPath = Instances.ProjectPathsOperator.GetServicesInterfacesDirectoryPath(projectDirectoryPath);
+
+            var serviceInterfacesDirectoryFilePaths = Instances.FileSystemOperator.EnumerateAllDescendentFilePathsOrEmptyIfNotExists(projectServiceInterfacesDirectoryPath);
+
+            var output = serviceInterfacesDirectoryFilePaths.ToArray();
+            return output;
+        }
+
+        public static Func<string, string[]> GetServiceInterfaceDirectoryFilePathsGenerator(this ICodeDirectoryOperator _)
+        {
+            string[] Output(string projectDirectoryPath) => _.GetServiceInterfaceDirectoryFilePaths(projectDirectoryPath);
+            return Output;
+        }
+
+        public static string[] GetBasesIntefacesDirectoryFilePaths(this ICodeDirectoryOperator _,
+            string projectDirectoryPath)
+        {
+            var projectBasesIntefacesDirectoryPath = Instances.ProjectPathsOperator.GetBasesInterfacesDirectoryPath(projectDirectoryPath);
+
+            var basesInterfacesDirectoryFilePaths = Instances.FileSystemOperator.EnumerateAllDescendentFilePathsOrEmptyIfNotExists(projectBasesIntefacesDirectoryPath);
+
+            var output = basesInterfacesDirectoryFilePaths.ToArray();
+            return output;
+        }
+
+        public static Func<string, string[]> GetBasesIntefacesDirectoryFilePathsGenerator(this ICodeDirectoryOperator _)
+        {
+            string[] Output(string projectDirectoryPath) => _.GetBasesIntefacesDirectoryFilePaths(projectDirectoryPath);
+            return Output;
+        }
+
+        public static string[] GetBasesExtensionsDirectoryFilePaths(this ICodeDirectoryOperator _,
+            string projectDirectoryPath)
+        {
+            var projectBasesExtensionsDirectoryPath = Instances.ProjectPathsOperator.GetBasesExtensionsDirectoryPath(projectDirectoryPath);
+
+            var basesExtensionsDirectoryFilePaths = Instances.FileSystemOperator.EnumerateAllDescendentFilePathsOrEmptyIfNotExists(projectBasesExtensionsDirectoryPath);
+
+            var output = basesExtensionsDirectoryFilePaths.ToArray();
+            return output;
+        }
+
+        public static Func<string, string[]> GetBasesExtensionsDirectoryFilePathsGenerator(this ICodeDirectoryOperator _)
+        {
+            string[] Output(string projectDirectoryPath) => _.GetBasesExtensionsDirectoryFilePaths(projectDirectoryPath);
+            return Output;
+        }
+    }
+}
--- source/R5T.T0045.X0001/Code/Instances.cs
+++ source/R5T.T0045.X0001/Code/Instances.cs
@@ -4,6 +4,8 @@ using R5T.L0011.T001;
 using R5T.T0034;
 using R5T.T0035;
 using R5T.T0036;
+using R5T.T0040;
+using R5T.T0044;
 
 
 namespace R5T.T0045.X0001
@@ -16,12 +18,14 @@ namespace R5T.T0045.X0001
         public static ICompilationUnitGenerator CompilationUnitGenerator { get; } = T0045.CompilationUnitGenerator.Instance;
         public static IDocumentationGenerator DocumentationGenerator { get; } = T0045.DocumentationGenerator.Instance;
         public static IDocumentationLine DocumentationLine { get; } = T0036.DocumentationLine.Instance;
+        public static IFileSystemOperator FileSystemOperator { get; } = T0044.FileSystemOperator.Instance;
         public static IIndentation Indentation { get; } = T0036.Indentation.Instance;
         public static IInterfaceGenerator InterfaceGenerator { get; } = T0045.InterfaceGenerator.Instance;
         public static IInterfaceName InterfaceName { get; } = T0036.InterfaceName.Instance;
         public static IMethodGenerator MethodGenerator { get; } = T0045.MethodGenerator.Instance;
         public static INamespacedTypeName NamespacedTypeName { get; } = T0034.NamespacedTypeName.Instance;
         public static INamespaceName NamespaceName { get; } = T0035.NamespaceName.Instance;
+        public static IProjectPathsOperator ProjectPathsOperator { get; } = T0040.ProjectPathsOperator.Instance;
         public static IPropertyGenerator PropertyGenerator { get; } = T0045.PropertyGenerator.Instance;
         public static IPropertyName PropertyName { get; } = T0036.PropertyName.Instance;
         public static IRegionName RegionName { get; } = T0036.RegionName.Instance;
