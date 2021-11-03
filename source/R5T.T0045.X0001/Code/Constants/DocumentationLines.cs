954e056e4a526d189cede38ab9e54b747fa5d381
David Coats <david@getsafetycone.com>
2021-11-03 15:51:22 -0700

    Update extension method base definition interface initial documentation line value.

--- source/R5T.T0045.X0001/Code/Constants/DocumentationLines.cs
+++ source/R5T.T0045.X0001/Code/Constants/DocumentationLines.cs
@@ -6,6 +6,6 @@ namespace R5T.T0045.X0001
     public static class DocumentationLines
     {
         public static string EmptyExtensionsBaseImplementation => "Empty implementation as base for extension methods.";
-        public static string EmptyExtensionsBaseInterface => "Empty interface as base for extension methods.";
+        public static string EmptyExtensionsBaseInterface => "Base for extensions ...";
     }
 }
